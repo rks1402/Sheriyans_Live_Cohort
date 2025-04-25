7d22d1cbf48a54267f603f9adea4670d5f9cb168
Rahul Kumar Sharma <63785638+rks1402@users.noreply.github.com>
2025-04-25 22:41:13 +0530

    added new project TO DO List App

---
+++ Projects/To Do List App - on going/script.js
@@ -0,0 +1,23 @@
+let addTaskBtn = document.querySelector("#addTaskBtn");
+let taskModal = document.querySelector("dialog");
+
+addTaskBtn.addEventListener("click", () => {
+    taskModal.showModal();
+})
+
+
+let todoSection = document.querySelector("#section1");
+let addTaskTitle = document.querySelector("#addTaskTitle");
+let addTaskDescription = document.querySelector("#addTaskDescription");
+let dialogAddBtn = document.querySelector("#dialogAddBtn");
+
+dialogAddBtn.addEventListener("click", () => {
+    let taskDiv = document.createElement("div");
+    taskDiv.setAttribute("class", "task");
+    let taskTitle = document.createElement("h5");
+    taskTitle.textContent = addTaskTitle.value;
+    let taskDescription = document.createElement("p");
+    taskDescription.textContent = addTaskDescription.value;
+    taskDiv.append(taskTitle, taskDescription);
+    todoSection.appendChild(taskDiv);
+})
